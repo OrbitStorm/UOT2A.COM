e067e5cab8017097abf27d2473af8e08cf2b2761
Grimoric <grimoric@hotmail.com>
2016-12-13 03:31:46 +0100

    Removed the BOD system

--- Scripts/Mobiles/Vendors/PlayerVendor.cs
+++ Scripts/Mobiles/Vendors/PlayerVendor.cs
@@ -127,7 +127,7 @@ namespace Server.Mobiles
 			if ( !base.CheckItemUse( from, item ) )
 				return false;
 
-			if ( item is Container || item is Engines.BulkOrders.BulkOrderBook )
+			if ( item is Container )
 				return true;
 
 			from.SendLocalizedMessage( 500447 ); // That is not accessible.
@@ -1505,7 +1505,7 @@ namespace Server.Mobiles
 						else
 							setPrice = true;
 					}
-					else if ( item is BaseBook || item is Engines.BulkOrders.BulkOrderBook )
+					else if ( item is BaseBook )
 					{
 						setPrice = true;
 					}
